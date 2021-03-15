3d08a09f6e7f0279b3e6a1ec569830506a431d90
innc11 <pm@innc11.cn>
2021-03-15 16:07:19 +0800

    refactor&feature: 支持v1.4.0服务端的新的异常信息传递机制，并重构了所有IO函数为async，替换了jq的ajax为原生的fetch函数

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import Vue from 'vue'
-import { MissingNecessaryFieldError, ServerSideError } from "./exception"
 import indexvue from './vaComment.vue'
 import { CreateElement } from 'vue/types/umd'
 import CommentingModel from './model/commentingModel'
 import CommentModel from './model/commentModel'
+import MissingNecessaryFieldError from './exception/MissingNecessaryFieldError'
+import IsServerSideError from './exception/IsServerSideError'
+import ServerSideException from './exception/ServerSideException'
 const $ = require('jquery')
 const cookies = require('brownies')
 const uaparser = require('ua-parser-js');
@@ -42,7 +44,7 @@ export default class VaComment
         this.captchaEnabled = config.captchaEnabled!=null? config.captchaEnabled:this.captchaEnabled
     }
 
-    create()
+    async create()
     {
         this.index = new Vue({
             el: '#'+this.elementId,
@@ -64,9 +66,9 @@ export default class VaComment
 
         this.loadCookies()
         
-        this.refresh()
+        await this.refresh()
 
-        this.smilies()
+        await this.smilies()
     }
 
     destroy()
@@ -122,7 +124,7 @@ export default class VaComment
     {
         if (typeof document === 'undefined')
         {
-            throw new ServerSideError()
+            throw new IsServerSideError()
         }
     }
 
@@ -143,138 +145,181 @@ export default class VaComment
         cookies.cookies.va_website = this.editor.formData.website
     }
 
+    async fetch2(input: RequestInfo, init?: RequestInit): Promise<any>
+    {
+        if(typeof(init)=='undefined')
+            init = {}
+            
+        let defaultOptions = {
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'include',
+        } as any
+
+        // 加载默认参数
+        let _init = init as any
+        for (const obj in defaultOptions)
+            if(!_init[obj])
+                _init[obj] = defaultOptions[obj]
+
+
+        let response = await fetch(input, init)
+
+        if (response.headers.has('X-Extra-Message')) 
+        {
+            let em = response.headers.get('X-Extra-Message');
+            let str = Buffer.from(em, 'base64').toString()
+            throw new ServerSideException(str)
+        }
+
+        let raw = await response.text()
+
+        try {
+            return JSON.parse(raw)
+        } catch (e) {
+            if(e instanceof SyntaxError)
+                return raw
+            return null;
+        }
+    }
+
     // 获取评论
-    refresh()
+    async refresh()
     {
         // 打开加载动画s
         this.index.isLoading = true
 
         let url = `${this.api}/comment?key=${this.key}&pagination=${this.index.pagination_current}&label=${this.pageLabel}`
 
-        $.ajax({
-            url: url,      async:    true,    type: 'GET',
-            cache: false,  dataType: "json",  
-            xhrFields: { withCredentials: true }, // 必须带上cookie
-            success: (json: any) => {
-                function sortfun(obj1: any, obj2: any) {
-                    if(obj1.time > obj2.time) return 1
-                    if(obj1.time < obj2.time) return -1
-                    return 0
-                }
+        try {
+            let json = await this.fetch2(url, {
+                method: 'GET'
+            })
 
-                function parseData(comments: any[])
-                {
-                    let allcomments = [] as Array<CommentModel>
-
-                    for (let comment of comments)
-                    {
-                        allcomments.push({
-                            id: comment.id,
-                            avatar: comment.avatar,
-                            nick: comment.nick,
-                            website: comment.website,
-                            isauthor: comment.isauthor,
-                            authorlabel: comment.authorlabel,
-                            ua: uaparser(comment.useragent),
-                            time: comment.time,
-                            content: comment.content,
-                            replies: parseData(comment.replies.slice(0).sort(sortfun))
-                        } )
-                    }
-
-                    return allcomments
-                }
-
-                // 加载(并显示)评论数据
-                this.index.allComments = parseData(json.comments) as CommentModel[]
-
-                // 加载分页数据
-                this.index.pagination_total = json.pages
-
-                // 设置评论数量
-                this.index.commentCount = json.count
-
-                // 隐藏加载动画
-                this.index.isLoading = false
-            },
-            error: (xhr: any, status: any, error: any) => {
-                console.log('评论获取失败: ')
-                console.log(xhr)
-                this.editor.showAlert('评论获取失败<br/>原因：'+status)
-
-                this.index.isLoading = false
+            let sortfun = function(obj1: any, obj2: any) {
+                if(obj1.time > obj2.time) return 1
+                if(obj1.time < obj2.time) return -1
+                return 0
             }
-        })
+    
+            let parseData = function(comments: any[])
+            {
+                let allcomments = [] as Array<CommentModel>
+    
+                for (let comment of comments)
+                {
+                    allcomments.push({
+                        id: comment.id,
+                        avatar: comment.avatar,
+                        nick: comment.nick,
+                        website: comment.website,
+                        isauthor: comment.isauthor,
+                        authorlabel: comment.authorlabel,
+                        ua: uaparser(comment.useragent),
+                        time: comment.time,
+                        content: comment.content,
+                        replies: parseData(comment.replies.slice(0).sort(sortfun))
+                    } )
+                }
+    
+                return allcomments
+            }
+    
+            // 加载(并显示)评论数据
+            this.index.allComments = parseData(json.comments) as CommentModel[]
+    
+            // 加载分页数据
+            this.index.pagination_total = json.pages
+    
+            // 设置评论数量
+            this.index.commentCount = json.count
+        } catch(e) {
+            if(e.name == 'ServerSideException')
+            {
+                this.editor.showAlert('评论获取失败<br/>原因：'+e.message)
+            } else {
+                this.editor.showAlert('评论获取失败<br/>原因：网络连接失败或者协议问题无法与服务端通信')
+            }
+            
+            this.index.isLoading = false
+        }
+
+        
+
+        // 隐藏加载动画
+        this.index.isLoading = false
     }
 
     // 提交评论
-    submit(comment: CommentingModel)
+    async submit(comment: CommentingModel)
     {
         comment.parent = this.index.replyId
         comment.key = this.key
         comment.label = this.pageLabel
 
-        let URL = this.api+'/comment'
-        $.ajax({
-            url: URL,      async:    true,    type: 'POST',
-            cache: false,  dataType: "json",  data: JSON.stringify(comment),
-            xhrFields: { withCredentials: true }, // 必须带上cookie
-            success: (res: any) => {
-                this.editor.formData.content = ''
-                this.editor.$emit('cancel-reply')
-                this.storageCookies()
-                this.editor.refreshCaptcha() // 刷新验证码
-                this.refresh() // 刷新评论
-            },
-            error: (xhr: any, status: any,error: any) => {
-                if (xhr.status!=200)
-                {
-                    console.log('发表评论失败: ')
-                    console.log(xhr)
-                    let reason = xhr.responseJSON.reason
-                    this.editor.showAlert('发表评论失败<br/>原因：'+(reason? reason:xhr.responseText))
-                } 
+        let url = this.api+'/comment'
+
+        try {
+            await this.fetch2(url, {
+                method: 'POST',
+                body: JSON.stringify(comment),
+            })
+
+            this.editor.formData.content = ''
+            this.editor.$emit('cancel-reply')
+            this.storageCookies()
+            this.editor.refreshCaptcha() // 刷新验证码
+            await this.refresh() // 刷新评论
+        } catch(e) {
+            if(e.name == 'ServerSideException')
+            {
+                this.editor.showAlert('发表评论失败<br/>原因：'+e.message)
+            } else {
+                this.editor.showAlert('发表评论失败<br/>原因：网络连接失败或者协议问题无法与服务端通信')
             }
-        })
+        }
     }
 
     // 获取表情
-    smilies()
+    async smilies()
     {
-        $.ajax({
-            url: this.api+'/smilie_api',   async:  true,    
-            cache: false,  dataType: "json",  type:  'GET',
-            xhrFields: { withCredentials: true }, // 必须带上cookie
-            success: (res: any) => {
-                let allSmilieSets = res[1]
-                for (let sm of allSmilieSets) {
-                    let smilieSet = sm[0]
-                    this.editor.smiliesComponet.smilies[smilieSet] = {}
+        let url = this.api+'/smilie_api'
 
-                    let url2 = this.api+'/smilie_api/'+smilieSet
-                    // console.log(url2)
-                    $.ajax({
-                        url: url2,   async:  true,    
-                        cache: false,  dataType: "json",  type:  'GET',
-                        xhrFields: { withCredentials: true }, // 必须带上cookie
-                        success: (res2: any) => {
-                            let urlHeader = res2[0]
-                            let allSmilies = res2[1]
-                            for (let sm2 of allSmilies) {
-                                let smiliesName = sm2;
-                                this.editor.smiliesComponet.smilies[smilieSet][smiliesName] = urlHeader+smilieSet+'/'+smiliesName
-                                this.editor.smiliesComponet.$forceUpdate()
-                                this.editor.smiliesComponet.defaultSmilieSet()
-                            }
-                        }
-                    })
+        try {
+                
+            let res = await this.fetch2(url, {
+                method: 'GET'
+            })
+
+            let allSmilieSets = res[1]
+            for (let sm of allSmilieSets) {
+                let smilieSet = sm[0]
+                this.editor.smiliesComponet.smilies[smilieSet] = {}
+
+                let url2 = this.api+'/smilie_api/'+smilieSet
+                
+                let res2 = await this.fetch2(url2, {
+                    method: 'GET'
+                })
+
+                let urlHeader = res2[0]
+                let allSmilies = res2[1]
+                for (let sm2 of allSmilies) {
+                    let smiliesName = sm2;
+                    this.editor.smiliesComponet.smilies[smilieSet][smiliesName] = urlHeader+smilieSet+'/'+smiliesName
+                    this.editor.smiliesComponet.$forceUpdate()
+                    this.editor.smiliesComponet.defaultSmilieSet()
                 }
-            },
-            error: (xhr: any, status: any,error: any) => {
-                    console.log('获取表情失败: ')
-                    console.log(xhr)
             }
-        })
+
+        } catch(e) {
+            if(e.name == 'ServerSideException')
+            {
+                this.editor.showAlert('获取表情失败<br/>原因：'+e.message)
+            } else {
+                this.editor.showAlert('获取表情失败<br/>原因：网络连接失败或者协议问题无法与服务端通信')
+            }
+        }
     }
 
     getCaptchaAPI()
@@ -293,5 +338,5 @@ export default class VaComment
     }
 }
 
-// 注册到全局变量
+// 暴露到全局变量
 window.VaComment = VaComment
